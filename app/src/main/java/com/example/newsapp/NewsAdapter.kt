287fac0971378ac2fa33d7b08a510e4aa3ef0ea1
AyushSantri <ayushsantri222@gmail.com>
2022-04-24 23:55:28 +0530

    created adapter and view holder

---
+++ app/src/main/java/com/example/newsapp/NewsAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter: RecyclerView.Adapter<> {
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
